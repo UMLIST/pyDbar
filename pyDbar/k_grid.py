2da34d20ebe9e44dcc1a26cdc92ad7202430c349
Jesen Tanadi <mail@jesentanadi.com>
2025-05-23 17:53:12 -0400

    use custom dtype for k_grid.grid, keeping track of coords and values within the same object

--- pyDbar/k_grid.py
+++ pyDbar/k_grid.py
@@ -8,13 +8,13 @@ class KGrid:
     """
     A grid object in the complex plane with the following parameters
     
-    grid: NDArray - [2M x 2M] grid points
+    grid: NDArray - [2M x 2M] points of type grid_type (see below)
     h: float      - Step size: h = (2*r)/(m-1)
     r: float      - Radius of support
     m: int        - The power in M = 2^m grid points
     M: int        - The unextended number of grid points M = 2^m
     """
-    grid: NDArray[np.complexfloating]
+    grid: NDArray
     h: float
     r: float
     m: int
@@ -45,13 +45,18 @@ def generate_kgrid(r: float, m: int) -> KGrid:
     # This is explained in "D-bar Demystified"
     s = r + ((M * r) / (M - 1))
 
+    # Declare our custom data type, which tracks both coordinates
+    # and t^exp approximation
+    grid_type = np.dtype([("coords", complex), ("texp", complex)])
+
     # The extension results in 2M x 2M grids
-    grid = np.zeros((2 * M, 2 * M), dtype=complex)
+    grid = np.zeros((2 * M, 2 * M), dtype=grid_type)
     x = np.arange(start=-s, stop=s + h, step=h)
 
+    # TODO: Move to using meshgrid
     for j in range(2 * M):
         for n in range(2 * M):
-            grid[j, n] = complex(x[j], x[n])
+            grid[j, n]["coords"] = complex(x[j], x[n])
 
     return KGrid(grid=grid, h=h, r=r, m=m, M=M)
 
@@ -60,20 +65,20 @@ if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
     k = generate_kgrid(m=4, r=2)
-    grid = k.grid
+    grid_coords = k.grid["coords"]
     r = k.r
     eps = 0.1
 
     print(f"Number of unextended points: {k.M} x {k.M}")
-    print(f"Number of actual (extended) points: {grid.shape[0]} x {grid.shape[0]}")
+    print(f"Number of actual (extended) points: {grid_coords.shape[0]} x {grid_coords.shape[0]}")
 
     # Extract points in disk
-    x_disk = np.real(grid[np.abs(grid) <= r + eps])
-    y_disk = np.imag(grid[np.abs(grid) <= r + eps])
+    x_disk = np.real(grid_coords[np.abs(grid_coords) <= r + eps])
+    y_disk = np.imag(grid_coords[np.abs(grid_coords) <= r + eps])
 
     # Extract points outside of disk
-    x_not_disk = np.real(grid[np.abs(grid) > r + eps])
-    y_not_disk = np.imag(grid[np.abs(grid) > r + eps])
+    x_not_disk = np.real(grid_coords[np.abs(grid_coords) > r + eps])
+    y_not_disk = np.imag(grid_coords[np.abs(grid_coords) > r + eps])
 
     # Plot
     fig, ax = plt.subplots()
