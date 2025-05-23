403eca645b42f8d383b1ada4fe9dc51735d20df8
Jesen Tanadi <mail@jesentanadi.com>
2025-05-23 12:11:18 -0400

    use custom data class for KGrid, instead of dict

--- pyDbar/k_grid.py
+++ pyDbar/k_grid.py
@@ -1,6 +1,27 @@
 import numpy as np
+from dataclasses import dataclass
 
-def generate_kgrid(r: float, m: int) -> dict:
+from numpy.typing import NDArray
+
+@dataclass
+class KGrid:
+    """
+    A grid object in the complex plane with the following parameters
+    
+    grid: NDArray - [2M x 2M] grid points
+    h: float      - Step size: h = (2*r)/(m-1)
+    r: float      - Radius of support
+    m: int        - The power in M = 2^m grid points
+    M: int        - The unextended number of grid points M = 2^m
+    """
+    grid: NDArray[np.complexfloating]
+    h: float
+    r: float
+    m: int
+    M: int
+
+
+def generate_kgrid(r: float, m: int) -> KGrid:
     """
     Generate square k-grid in z-space. The number of grid points
     is (2M)^2, where M = 2^m. A disk of radius r is drawn inside of
@@ -9,18 +30,12 @@ def generate_kgrid(r: float, m: int) -> dict:
     disk, there are M/2 points on each side. The step size between
     each grid point is h = (2*r)/(M-1).
 
-    The returned object is a dictionary with keys listed below.
-
     Input:
     r: float - Radius of support
     m: int   - Value for M = 2^m, the number of grids generated
 
-    Output parameters:
-    grid: NDArray - [M x M] grid points
-    h: float      - Step size: h = (2*r)/(m-1)
-    r: float      - Radius of support
-    m: int        - The power in M = 2^m grid points
-    M: int        - The unextended number of grid points M = 2^m
+    Output:
+    kgrid: KGrid - Object with grid and other relevant parameters (see above)
     """
     M = 2**m # Number of (unextended) grid points
     h = (2 * r) / (M - 1) # Step size
@@ -38,26 +53,18 @@ def generate_kgrid(r: float, m: int) -> dict:
         for n in range(2 * M):
             grid[j, n] = complex(x[j], x[n])
 
-    k_grid = {
-        "grid": grid,
-        "h": h,
-        "r": r,
-        "m": m,
-        "M": M
-    }
-
-    return k_grid
+    return KGrid(grid=grid, h=h, r=r, m=m, M=M)
 
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
     k = generate_kgrid(m=4, r=2)
-    grid = k["grid"]
-    r = k["r"]
+    grid = k.grid
+    r = k.r
     eps = 0.1
 
-    print(f"Number of unextended points: {k['M']} x {k['M']}")
+    print(f"Number of unextended points: {k.M} x {k.M}")
     print(f"Number of actual (extended) points: {grid.shape[0]} x {grid.shape[0]}")
 
     # Extract points in disk
